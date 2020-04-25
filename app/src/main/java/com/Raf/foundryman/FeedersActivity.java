8298d957a86039fea1e06eb489699d4d4190476a
Rafal Urbanek <rafal.m.urbanek@gmail.com>
2020-04-25 23:32:55 +0100

    MAIN - Created the Feeders layout
    To Do:
    - Add the data values corresponding to newly implemented fields in the main panel
    - Unit test the calculator
    - finish adapter application to the Feeders view

--- app/src/main/java/com/Raf/foundryman/FeedersActivity.java
+++ app/src/main/java/com/Raf/foundryman/FeedersActivity.java
@@ -3,7 +3,9 @@ package com.Raf.foundryman;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -12,6 +14,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class FeedersActivity extends AppCompatActivity implements
         AdapterView.OnItemSelectedListener {
@@ -20,6 +24,9 @@ public class FeedersActivity extends AppCompatActivity implements
     Spinner toolSpinner;
     Boolean flag;
     TextView topText;
+    private RecyclerView feederList;
+    private RecyclerView.Adapter feederListAdapter;
+    private RecyclerView.LayoutManager feederListLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +35,28 @@ public class FeedersActivity extends AppCompatActivity implements
         flag = false;
         tools = getResources().getStringArray(R.array.tools);
         topText = findViewById(R.id.summaryTxtFeeders);
+        feederList = findViewById(R.id.FeederRecycler);
 
         configureOptionsBtn();
         configureProjectText();
         configureSpinner();
 
         topText.setText(tools[5].toUpperCase());
+
+        setFeederList();
+
+    }
+
+    private void setFeederList() {
+        feederList.setHasFixedSize(true);
+
+        // use a linear layout manager
+        feederListLayoutManager = new LinearLayoutManager(this);
+        feederList.setLayoutManager(feederListLayoutManager);
+
+        // specify an adapter (see also next example)
+        feederListAdapter = new MyAdapter(myDataset);
+        feederList.setAdapter(feederListAdapter);
     }
 
     private void configureSpinner() {
@@ -79,6 +102,52 @@ public class FeedersActivity extends AppCompatActivity implements
 
     @Override
     public void onNothingSelected(AdapterView<?> arg0) {
+    }
+}
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView textView;
+        public MyViewHolder(TextView v) {
+            super(v);
+            textView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
     }
 }
 
